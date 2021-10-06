f0d85f3df6f9b6b3ca63eb42b26ade1159b0d3fc
Alexandre Bisognin <alexandrefb.86@gmail.com>
2021-10-06 17:19:14 +0100

    Criando o código JS

---
+++ 04-js-basico/03-passar-a-noite/index.js
@@ -0,0 +1,7 @@
+let confirmacao = window.confirm("Seja bem vindo(a), aventureiro(a)! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?");
+
+if (confirmacao == true) {
+    window.alert("Ótimo! Nós temos as melhores camas de toda a região!");
+} else {
+    window.alert("Que pena! Você parecia ser uma pessoa mais legal");
+}
